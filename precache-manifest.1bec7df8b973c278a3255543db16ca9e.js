3e9bba2c1465f65c0b449fa98c664f790bc2e4a7
Haozhe Zhou <zhou929@purdue.edu>
2021-09-20 16:50:00 -0400

    Updates

---
+++ precache-manifest.1bec7df8b973c278a3255543db16ca9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52217fdb8c79056a629a7e046bfe533f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2190389a64d97314e69c",
+    "url": "/static/css/main.b1f3bdcd.chunk.css"
+  },
+  {
+    "revision": "a162d5e4801e8befc84a",
+    "url": "/static/js/2.f1719020.chunk.js"
+  },
+  {
+    "revision": "86d9334d3f5c9b19a41c29af348390d5",
+    "url": "/static/js/2.f1719020.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2190389a64d97314e69c",
+    "url": "/static/js/main.36bc23c5.chunk.js"
+  },
+  {
+    "revision": "7b006285db19d54a49c7",
+    "url": "/static/js/runtime-main.08323b0d.js"
+  },
+  {
+    "revision": "680982e6aa58367e124d460071324e6f",
+    "url": "/static/media/naafi_photo.680982e6.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/website/precache-manifest.47eb56123fa99760b0c188a59affaac1.js"
+  "/precache-manifest.1bec7df8b973c278a3255543db16ca9e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
